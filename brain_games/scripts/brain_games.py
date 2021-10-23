36c6b19f34072eb1edcaff2a5e006a80317564cd
Sam Jonov <onfezy@gmail.com>
2021-10-23 14:23:48 +0300

    Create cli.py and modify brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
+from brain_games import cli
+
 def greet():
     print('Welcome to the Brain Games!')
 
 def main():
     greet()
+    welcome_user()
 
 if __name__ == '__main__':
     main()
